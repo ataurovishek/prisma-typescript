703e1f63d314dfcf6574c549994b545cab6991c0
ataur rahman <ataurovishek@gmail.com>
2025-03-14 02:41:51 +0600

    change

--- script.ts
+++ script.ts
@@ -1,31 +1,53 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({log:['query']});
+const prisma = new PrismaClient();
 
 
 async function main() {
     await prisma.user.deleteMany()
-    const user = await prisma.user.create({
-        data: {
-            name: 'ataur rahman',
-            email: 'ataurovishek@gmail.com.',
-            age: 27,
-            userPreference: {
-                create: {
-                    emailUpdates: true
-                }
+
+
+    // this is only for creating one user 
+
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: 'ataur rahman',
+    //         email: 'ataurovishek@gmail.com.',
+    //         age: 27,
+    //         userPreference: {
+    //             create: {
+    //                 emailUpdates: true
+    //             }
+    //         }
+    //     },
+    //     select:{
+    //         name:true,
+    //         userPreference:{
+    //             select:{
+    //                 id:true
+    //             }
+    //         }
+    //     }
+    // })
+    // console.log(user);
+
+    // this is for creating multiple users 
+    const user = await prisma.user.createMany({
+        data: [
+            {
+                name: 'ataur rahman',
+                email: 'ataurovishek@gmail.com',
+                age: 27
+
+            }, {
+                name: 'irfan rahman',
+                email: 'irfan@gmail.com',
+                age: 25
             }
-        },
-        select:{
-            name:true,
-            userPreference:{
-                select:{
-                    id:true
-                }
-            }
-        }
+        ]
     })
     console.log(user);
+
 }
 
 main().catch(e => {
