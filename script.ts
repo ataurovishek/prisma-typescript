0a8e13be99d12515ebfa305f3e813ce0189a53bb
ataur rahman <ataurovishek@gmail.com>
2025-03-14 01:10:58 +0600

    first commit

---
+++ script.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+
+async function main() {
+    await prisma.user.deleteMany()
+    const user = await prisma.user.create({
+        data: {
+            name: 'ataur rahman',
+            email: 'ataurovishek@gmail.com.',
+            age: 27,
+            userPreference: {
+                create: {
+                    emailUpdates: true
+                }
+            }
+        },
+        include: {
+            userPreference: true
+        }
+    })
+    console.log(user);
+}
+
+main().catch(e => {
+    console.error(e.message);
+}).finally(async () => {
+    await prisma.$disconnect();
+})
